73bd488899a007126afb8eb54aa3aae438484227
George Gondim Ribeiro <gondimribeiro@gmail.com>
2015-11-07 19:37:01 -0200

    minor modifications

--- src/network3.py
+++ src/network3.py
@@ -101,7 +101,7 @@ class Network(object):
         self.output_dropout = self.layers[-1].output_dropout
 
     def SGD(self, training_data, epochs, mini_batch_size, eta,
-            validation_data, test_data, lmbda=0.0):
+            validation_data, test_data, lmbda=0.0, monitor_data=False):
         """Train the network using mini-batch stochastic gradient descent."""
         training_x, training_y = training_data
         validation_x, validation_y = validation_data
@@ -125,6 +125,14 @@ class Network(object):
         i = T.lscalar() # mini-batch index
         train_mb = theano.function(
             [i], cost, updates=updates,
+            givens={
+                self.x:
+                training_x[i*self.mini_batch_size: (i+1)*self.mini_batch_size],
+                self.y:
+                training_y[i*self.mini_batch_size: (i+1)*self.mini_batch_size]
+            })
+        training_mb_accuracy = theano.function(
+            [i], self.layers[-1].accuracy(self.y),
             givens={
                 self.x:
                 training_x[i*self.mini_batch_size: (i+1)*self.mini_batch_size],
@@ -155,17 +163,26 @@ class Network(object):
             })
         # Do the actual training
         best_validation_accuracy = 0.0
+        out_validation_accuracy, out_training_accuracy = [], []
         for epoch in xrange(epochs):
             for minibatch_index in xrange(num_training_batches):
                 iteration = num_training_batches*epoch+minibatch_index
-                if iteration % 1000 == 0:
-                    print("Training mini-batch number {0}".format(iteration))
+                #if iteration % 1000 == 0:
+                #    print("Training mini-batch number {0}".format(iteration))
                 cost_ij = train_mb(minibatch_index)
                 if (iteration+1) % num_training_batches == 0:
                     validation_accuracy = np.mean(
                         [validate_mb_accuracy(j) for j in xrange(num_validation_batches)])
-                    print("Epoch {0}: validation accuracy {1:.2%}".format(
-                        epoch, validation_accuracy))
+                    training_accuracy = np.mean(
+                            [training_mb_accuracy(j) for j in xrange(num_training_batches)])
+                    if monitor_data:
+                        out_validation_accuracy.append(validation_accuracy)
+                        out_training_accuracy.append(training_accuracy)
+
+                    print(("\nEpoch {0} || training accuracy {1:.2%} | "
+                          "validation accuracy {2:.2%}").format(
+                              epoch, training_accuracy, validation_accuracy))
+                    
                     if validation_accuracy >= best_validation_accuracy:
                         print("This is the best validation accuracy to date.")
                         best_validation_accuracy = validation_accuracy
@@ -175,10 +192,12 @@ class Network(object):
                                 [test_mb_accuracy(j) for j in xrange(num_test_batches)])
                             print('The corresponding test accuracy is {0:.2%}'.format(
                                 test_accuracy))
-        print("Finished training network.")
+                    
+        print("\nFinished training network.")
         print("Best validation accuracy of {0:.2%} obtained at iteration {1}".format(
             best_validation_accuracy, best_iteration))
         print("Corresponding test accuracy of {0:.2%}".format(test_accuracy))
+        return out_training_accuracy, out_validation_accuracy
 
 #### Define layer types
 
